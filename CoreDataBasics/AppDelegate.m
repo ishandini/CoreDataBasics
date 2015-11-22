c138f800359f8c055f1d7b04214aafa114835302
ishandini <ishandini@live.com>
2015-11-22 21:56:30 +0530

    another changes to appdelegate

--- CoreDataBasics/AppDelegate.m
+++ CoreDataBasics/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after launch.
+    // Override point for customization  launch.
     return YES;
 }
 
